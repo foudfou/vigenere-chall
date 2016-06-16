33ad7a93465d769d555dbbb5dd2caff1e606f927
Foudil BRÉTEL <foudil.bretel@cc.in2p3.fr>
2016-06-16 11:43:58 +0200

    More functional version.

--- src/main/scala/vigenere/Vigenere.scala
+++ src/main/scala/vigenere/Vigenere.scala
@@ -1,52 +1,40 @@
 /* Algebraic Vigenere cypher */
 
 object Vigenere {
+
+  def decrypt(secret: String, key: String, alpha: String) = {
+    val alen = alpha.length()
+    def decryptAux(pos: Int, sec: List[Char], clear: List[Char]): List[Char] =
+      sec match {
+        case Nil => clear
+        case s :: ss =>
+          val k = key.charAt(pos % key.length())
+
+          val si = alpha.indexOf(s)
+          val ki = alpha.indexOf(k)
+
+          val ci = (si - ki + alen) % alen // +alen for positive range
+          val c = alpha.charAt(ci)
+
+          decryptAux(pos + 1, ss, c :: clear)
+      }
+    val clearList = decryptAux(0, secret.toList, List())
+    clearList.reverse.mkString("")
+  }
+}
+
+object VigenerePuzzle {
   val Alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ. :/"
-  val AlphaLen = Alpha.length()
   val Key = "FOUDIL"  // 0x464F5544494C
   val Text  = "NLUPF/TLWRWWCVJWX/DNTJQAMEV/KZRNZR:OK:KC"
   val Clear = "I AM SO COOL HTTPS://GITHUB.COM/FOUDFOU/"
   //          "FOUDILFOUDILFOUDILFOUDILFOUDILFOUDILFOUD"
 
-  // Text[i] = (Crypted[i] - Key[i]) mod 30
-  def decrypt() = {
-    for (i <- 0 to Text.length() - 1) {
-      val tChar = Text.charAt(i)
-      val kChar = Key.charAt(i % Key.length())
-
-      val tInt = Alpha.indexOf(tChar)
-      val kInt = Alpha.indexOf(kChar)
-
-      val cInt = (tInt - kInt + AlphaLen) % AlphaLen // +AlphaLen for positive range
-      val cChar = Alpha.charAt(cInt)
-
-      println(s"$i: $tChar - $kChar = $cChar")
-      // print(cChar)
-    }
-  }
-
-  def encrypt(text: String) = {
-    for (i <- 0 to text.length() - 1) {
-      val tChar = text.charAt(i)
-      val kChar = Key.charAt(i % Key.length())
-
-      val tInt = Alpha.indexOf(tChar)
-      val kInt = Alpha.indexOf(kChar)
-
-      val cInt = (tInt + kInt) % AlphaLen
-      val cChar = Alpha.charAt(cInt)
-
-      print(cChar)
-    }
-  }
-}
-
-object VigenerePuzzle {
   def main(args: Array[String]): Unit = {
-    val secret = Vigenere.encrypt(Vigenere.Clear)
-    println()
-    val clear = Vigenere.decrypt()
+    val clear = Vigenere.decrypt(Text, Key, Alpha)
+    println(s"Text=$clear")
 
-    // println(s"Text=$clear")
+    // val secret = Vigenere.encrypt(Vigenere.Clear)
+    // println()
   }
 }
